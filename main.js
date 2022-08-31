e79ac27f23d119c748ff3a0b6ec45743fed06f6c
Enoch Aremu <110859297+enoch-doc@users.noreply.github.com>
2022-09-01 00:18:51 +0100

    added main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+var span = document.getElementById("close")[0];
+function closeIt() {
+if  (confirm ('Do you want to close this modal?')) {
+    modal.style.display = "none";
+} 
+}
+
+const logIn = () =>{
+    let userEmail = email.value
+    let userPsw = psw.value
+    
+}
